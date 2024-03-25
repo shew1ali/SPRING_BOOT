7360c59139ebba26a4f8efb43c30dac1d26606ea
ALina Shewczowa <shewczowa.alina@yandex.ru>
2024-03-25 03:06:44 +0300

    first

---
+++ src/main/java/Configuration/JavaConfig.java
@@ -0,0 +1,22 @@
+package Configuration;
+import Controller.DevProfile;
+import Controller.ProductionProfile;
+import Controller.SystemProfile;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JavaConfig {
+    @Bean
+    @ConditionalOnProperty(value ="netology.profile.dev", havingValue ="true")
+    public SystemProfile devProfile() {
+        return new DevProfile();
+    }
+
+    @Bean
+    @ConditionalOnProperty(value = "netology.profile.dev", havingValue = "false")
+    public SystemProfile prodProfile() {
+        return new ProductionProfile();
+    }
+}
---
+++ src/main/java/Controller/SystemProfile.java
@@ -0,0 +1,6 @@
+package Controller;
+
+public interface SystemProfile {
+    String getProfile();
+}
+
